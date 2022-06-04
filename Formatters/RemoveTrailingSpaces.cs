8cb61a4972ad5775394e06578c3d1ee7f1657947
Tinyfish <rgvuce@gmail.com>
2022-06-04 21:05:15 +0800

    Add RemoveTrailingSpaces.

---
+++ Formatters/RemoveTrailingSpaces.cs
@@ -0,0 +1,10 @@
+﻿namespace MarkdownFormatter;
+
+public class RemoveTrailingSpaces : BaseFormatter
+{
+    public override void Format(List<string> lines, string filePath)
+    {
+        for (var i = 0; i < lines.Count; i++)
+            lines[i] = lines[i].TrimEnd();
+    }
+}
